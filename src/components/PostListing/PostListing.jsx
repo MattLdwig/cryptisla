2bc4bf1a33e4111370d6809f68dd6e77615739d4
MattLdwig <mattldwig@gmail.com>
2019-04-25 09:28:37 +0200

    style($postListing): Switch layout three columns

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -28,21 +28,36 @@ class PostListing extends React.Component {
           const style = {background: 'url(' + post.cover + ')', backgroundPosition: 'top', backgroundSize: 'cover'}
           if (post === postList[0]) {
             return (
-            <Link to={post.path} key={`${post.title}_post`} className="post-card__link">
+            <Link to={post.path} key={`${post.title}_post`} className="post-card__link__hero">
               <div className="post-card__hero" style={style} key={post.title}>
+              <div class="post-card__overlay">
                 <p className="post-card__complementary">
                   <span className="post-card__tag">{post.tags}</span>
-                  <span className="post-card__date">{post.displayDate}</span>
                 </p> 
                 <div className="post-card__info">
                   <h1 className="post-card__hero-title">{post.title}</h1>
+                  <span className="post-card__date">{post.displayDate}</span>
                 </div>
-                <div className="post-card__more">
-                  <span>LIRE L'ARTICLE</span>
-                </div>
+              </div>
             </div>
           </Link>
             )   
+          } else if (post === postList[1] || post === postList[2]) {
+            return (
+              <Link to={post.path} key={`${post.title}_post`} className="post-card__link-col">
+              <div className="post-card__hero__col" style={style} key={post.title}>
+                <div class="post-card__overlay">
+                  <p className="post-card__complementary">
+                    <span className="post-card__tag">{post.tags}</span>
+                  </p> 
+                  <div className="post-card__info">
+                    <h1 className="post-card__hero-title">{post.title}</h1>
+                    <span className="post-card__date">{post.displayDate}</span>
+                  </div>
+                </div>
+            </div>
+              </Link>
+            )
           }
         })}
       </div>
